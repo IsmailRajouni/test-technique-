96dcb26a2069e4e79bea5e781d75e4173c7d28d1
Ismail.Rajouni <ismail.rajouni@ebp.com>
2023-11-10 17:15:19 +0100

    mot de passe

--- cypress/e2e/creation-mot-depasse.js
+++ cypress/e2e/creation-mot-depasse.js
@@ -38,6 +38,40 @@ describe("Vérifie que le mot de passe est suffisamment robuste", () => {
       });
     });
   });
+
+  // Teste la création d'un compte avec vérification de la robustesse du mot de passe
+it("Vérifie que le mot de passe est suffisamment robuste", () => {
+  // Visite la page d'accueil
+  cy.visit('/');
+
+  // Clique sur "Create an Account" dans la barre de navigation
+  cy.get('.header').contains('Create an Account').click();
+
+  // Vérifie que l'URL contient "/account/create/"
+  cy.url().should('contain', '/account/create/');
+
+  // Génère des valeurs aléatoires pour le nom, le prénom, l'e-mail et le mot de passe
+  const nom = 'user_' + Math.random().toFixed(5).split('.')[1];
+  const prenom  = Math.random().toString(36).substring(2, 7);
+  const email = prenom + '.' + nom + '@yopmail.com';
+  const password = Math.random().toString(36).substring(2, 15).replace(/^\w/, (c) => c.toUpperCase());
+
+  // Remplit les champs du formulaire avec les valeurs générées
+  cy.get('#firstname').type(prenom);
+  cy.get('#lastname').type(nom);
+  cy.get('#email_address').type(email);
+  cy.get('#password').type(password);
+  cy.get('#password-confirmation').type(password);
+
+  // Soumet le formulaire en cliquant sur le bouton "Submit"
+  cy.get('.actions-toolbar').get('.submit').click();
+
+  // Vérifie que l'URL contient "/account/" après la soumission du formulaire
+  cy.url().should('contain', '/account/');
+
+  // Vérifie la présence du message de succès après la création du compte
+  cy.get('.message-success').contains('Thank you for registering with Main Website Store.').should('exist');
+});
   
   
   
